92a34854cc6d296d785ac9412f00ff6d3ffb4015
RealDJS <dj@skoys.com>
2022-09-24 23:43:31 -0600

    updated for aboutMe part II

---
+++ aboutme/about-me.js
@@ -0,0 +1,27 @@
+console.log("It worked!")
+const favoriteFoods = [" steak", " ginger beer", " german pancakes"];
+
+const newP = document.createElement("p");
+
+newP.textContent = `My Favorite foods: ${favoriteFoods}`;
+
+document.body.appendChild(newP);
+
+
+const weekday = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+];
+
+console.log(weekday);
+const today = new Date();
+console.log(today);
+const day = today.getDay();
+console.log(day);
+const todayElement = document.querySelector("#displayToday");
+todayElement.textContent = weekday[day];
