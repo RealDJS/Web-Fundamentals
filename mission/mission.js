8bb8f5c813c00eebc538d8f30e21f526d0e64fa8
RealDJS <dj@skoys.com>
2022-10-19 13:01:55 -0600

    Fixed the Dark mode selector

--- mission/mission.js
+++ mission/mission.js
@@ -1,11 +1,21 @@
 // mission.js
-const themeSelector = document.getElementById("theme")
+const themeSelector = document.getElementById("theme-select")
 
-const checkValue = themeSelector.value;
+themeSelector.addEventListener("change", changeTheme)
 
 function changeTheme() {
     //check to see what the current value of our select is. The current value is conveniently found in themeSelector.value!
 
+    const checkValue = themeSelector.value;
+    console.log(checkValue)
+    if (checkValue == "Dark") {
+        document.body.classList.add("dark")
+        document.querySelector(".logo").src = "byui-logo_white.png"
+    }
+    else {
+        document.body.classList.remove("dark")
+        document.querySelector(".logo").src = "byui-logo_blue.jpg"
+    }
     // if the value is dark then:
     // add the dark class to the body
     // change the source of the logo to point to the white logo.
